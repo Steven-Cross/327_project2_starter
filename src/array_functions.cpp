5793df252e0499786275c3baefa986fd394c2186
Steven Cross <steven.cross.17@cnu.edu>
2019-09-26 20:09:14 -0700

    Skeleton Code

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -8,6 +8,7 @@
 //============================================================================
 //	TODO add necessary includes here
 #include "array_functions.h"
+#include <string>
 //============================================================================
 
 //============================================================================
@@ -20,5 +21,50 @@
 //TODO add variable to keep track of next available slot in array
 
 //TODO define all functions in header file
+void clearArray() {
+
+}
+
+int getArraySize(){
+	return -1;
+}
+
+std::string getArrayWordAt(int i){
+	return "TODO";
+}
+
+int getArrayWord_NumbOccur_At(int i){
+	return -1;
+}
+
+bool processFile(std::fstream &myfstream){
+	return false;
+}
+
+void processLine(std::string &myString){
+	return;
+}
+
+void processToken(std::string &token){
+	return;
+}
+
+bool openFile(std::fstream& myfile, const std::string& myFileName,
+		std::ios_base::openmode mode = std::ios_base::in) {
+	return false;
+}
+
+void closeFile(std::fstream& myfile){
+	return;
+}
+
+int writeArraytoFile(const std::string &outputfilename){
+	return -1;
+}
+
+void sortArray(constants::sortOrder so){
+	return;
+}
+
 
 //TODO look in utilities.h for useful functions, particularly strip_unwanted_chars!
